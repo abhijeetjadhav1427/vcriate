d0c5a17c0361e3be4f8f6f26bd9758d5213504fb
AbhijeetJadhav <abhijeet.jadhav@cognizant.com>
2023-07-09 11:04:45 +0530

    history tracking ui and api

--- src/main/java/com/vcriate/repository/UserRepository.java
+++ src/main/java/com/vcriate/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.vcriate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.vcriate.entity.User;
@@ -8,4 +10,7 @@ import com.vcriate.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
 	public User findByUsernameAndPassword(String username, String password);
+
+	@Query("SELECT u FROM User u LEFT JOIN FETCH u.history h WHERE u.id = :userId ORDER BY h.timestamp DESC")
+    public User findByIdWithSortedHistory(@Param("userId") int userId);
 }
--- src/main/java/com/vcriate/service/WalletService.java
+++ src/main/java/com/vcriate/service/WalletService.java
@@ -1,10 +1,16 @@
 package com.vcriate.service;
 
+import java.sql.Timestamp;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.vcriate.entity.History;
+import com.vcriate.entity.User;
 import com.vcriate.entity.Wallet;
 import com.vcriate.enums.WalletStatus;
+import com.vcriate.repository.HistoryRepository;
+import com.vcriate.repository.UserRepository;
 import com.vcriate.repository.WalletRepository;
 
 import jakarta.transaction.Transactional;
@@ -12,44 +18,63 @@ import jakarta.transaction.Transactional;
 @Service
 public class WalletService {
 	@Autowired
+	private UserRepository userRepository;
+	@Autowired
 	private WalletRepository walletRepository;
+	@Autowired
+	private HistoryRepository historyRepository;
 
 	private Wallet getWalletByUserId(int userId) {
 		return walletRepository.getWalletByUserId(userId);
 	}
-	
+
 	@Transactional
-	public WalletStatus creditToWallet(int userId, int amount) {		
+	public WalletStatus creditToWallet(int userId, int amount) {
 		Wallet wallet = getWalletByUserId(userId);
 		WalletStatus status = WalletStatus.CREDITED;
-		
+
 		if (wallet == null) {
 			status = WalletStatus.USER_NOT_FOUND;
-		}
-		else {
+		} else {
+			saveHistory(userId, "+" + Integer.toString(amount));
 			wallet.setAmount(wallet.getAmount() + amount);
-			walletRepository.save(wallet);	
+			walletRepository.save(wallet);
 		}
-		
+
 		return status;
 	}
-	
+
 	@Transactional
 	public WalletStatus debitFromWallet(int userId, int amount) {
 		Wallet wallet = walletRepository.getWalletByUserId(userId);
 		WalletStatus status = WalletStatus.DEBITED;
-		
+
 		if (wallet == null) {
 			status = WalletStatus.USER_NOT_FOUND;
-		}
-		else if(wallet.getAmount() < amount) {
+		} else if (wallet.getAmount() < amount) {
 			status = WalletStatus.INSUFFICIENT_BALANCE;
-		}
-		else {
-			wallet.setAmount(wallet.getAmount()-amount);
+		} else {
+			saveHistory(userId, "-" + Integer.toString(amount));
+			wallet.setAmount(wallet.getAmount() - amount);
 			walletRepository.save(wallet);
 		}
-		
+
 		return status;
+	}
+
+	@Transactional
+	private WalletStatus saveHistory(int userId, String amount) {
+		User user = userRepository.findById(userId).orElse(null);
+		if (user == null) {
+			return WalletStatus.USER_NOT_FOUND;
+		}
+		
+		History history = new History();
+		history.setUser(user);
+		history.setAmount(amount);
+		history.setTimestamp(new Timestamp(System.currentTimeMillis()));
+		historyRepository.save(history);
+		
+		return WalletStatus.HISTORY_SAVED;
 	}
 }
